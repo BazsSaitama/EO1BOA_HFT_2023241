5f3e56f35f881a7779816552ab0d9e74de2564bc
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2023-12-11 12:45:26 +0100

    OvenRepository(Read,Update) extends Generic IRepository

---
+++ EO1BOA_HFT_2023241.Repository/OvenRepository.cs
@@ -0,0 +1,30 @@
+﻿using EO1BOA_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EO1BOA_HFT_2023241.Repository
+{
+    public class OvenRepository : Repository<Oven>
+    {
+        public OvenRepository(BakeryDbContext ctor) : base(ctor)
+        {
+        }
+
+        public override Oven Read(int id)
+        {
+            return dbcontext.Ovens.FirstOrDefault(x => x.OvenId == id);
+        }
+
+        public override void Update(Oven obj)
+        {
+            var old = Read(obj.OvenId);
+            foreach (var item in old.GetType().GetProperties())
+            {
+                item.SetValue(old, item.GetValue(obj));
+            }
+        }
+    }
+}
