80b39bace2e293d6740e6632623ac0c4310987a8
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2023-12-11 18:57:30 +0100

    OvenLogic with  Exceptionhandling

--- EO1BOA_HFT_2023241.Logic/OvenLogic.cs
+++ EO1BOA_HFT_2023241.Logic/OvenLogic.cs
@@ -1,5 +1,6 @@
 ﻿using EO1BOA_HFT_2023241.Logic.Interfaces;
 using EO1BOA_HFT_2023241.Models;
+using EO1BOA_HFT_2023241.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +11,44 @@ namespace EO1BOA_HFT_2023241.Logic
 {
     public class OvenLogic : IOvenLogic
     {
+        IRepository<Oven> repo;
+
+        public OvenLogic(IRepository<Oven> repo)
+        {
+            this.repo = repo;
+        }
+
         public void Create(Oven oven)
         {
-            throw new NotImplementedException();
+            if (oven.Price < 0 || oven.BreadCapacity < 0 || oven.BakingTime < 0 )
+            {
+                throw new ArgumentException();
+            }
+            this.repo.Create(oven);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            if (repo.Read(id) == null)
+            {
+                throw new Exception();
+            }
+            this.repo.Delete(id);
         }
 
         public Oven Read(int id)
         {
-            throw new NotImplementedException();
+            return this.repo.Read(id);
         }
 
         public IQueryable<Oven> ReadAll()
         {
-            throw new NotImplementedException();
+            return this.repo.ReadAll();
         }
 
         public void Update(Oven oven)
         {
-            throw new NotImplementedException();
+            this.repo.Update(oven);
         }
     }
 }
