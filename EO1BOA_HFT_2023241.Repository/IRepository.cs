162ad6343cec6b8988c73bda98aeb576a9b3a553
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2023-12-11 08:22:52 +0100

    Created IRepository interface with CRUD methods

---
+++ EO1BOA_HFT_2023241.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EO1BOA_HFT_2023241.Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        void Create(T obj);
+        T Read(int id);
+        void Update(T obj);
+        void Delete(int id);
+
+        IQueryable<T> ReadAll();
+    }
+}
