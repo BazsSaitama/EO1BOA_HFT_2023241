0b0f199ee8ecef6e6ef38a2bcbd663be9fa5074d
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2023-12-12 12:48:06 +0100

    Endpoint startup ConfigureServices

--- EO1BOA_HFT_2023241.Endpoint/Startup.cs
+++ EO1BOA_HFT_2023241.Endpoint/Startup.cs
@@ -1,8 +1,14 @@
+using EO1BOA_HFT_2023241.Logic;
+using EO1BOA_HFT_2023241.Logic.Interfaces;
+using EO1BOA_HFT_2023241.Models;
+using EO1BOA_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +18,30 @@ namespace EO1BOA_HFT_2023241.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get ; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<BakeryDbContext>();
+            services.AddTransient<IRepository<Bread>, BreadRepository>();
+            services.AddTransient<IRepository<Bakery>, BakeryRepository>();
+            services.AddTransient<IRepository<Oven>, OvenRepository>();
+            services.AddTransient<IBreadLogic, BreadLogic>();
+            services.AddTransient<IBakeryLogic, BakeryLogic>();
+            services.AddTransient<IOvenLogic, OvenLogic>();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "EO1BOA_HFT_2023241_Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
