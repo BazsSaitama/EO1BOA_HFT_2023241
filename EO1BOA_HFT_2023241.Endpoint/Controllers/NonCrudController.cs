acef1aa69d40c425a646e37e869073bbe4851b76
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2023-12-12 18:18:41 +0100

    Added NonCrudController

---
+++ EO1BOA_HFT_2023241.Endpoint/Controllers/NonCrudController.cs
@@ -0,0 +1,56 @@
+﻿using EO1BOA_HFT_2023241.Logic.Interfaces;
+using EO1BOA_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EO1BOA_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class NonCrudController : ControllerBase
+    {
+        IBakeryLogic logic;
+
+        public NonCrudController(IBakeryLogic logic)
+        {
+            this.logic = logic;
+        }
+        //public IQueryable<Oven> OvensByCapacity(int capacity);
+        //public IQueryable<Bread> AllBreadsFromBakery(string bakery);
+        //public Oven MostExpensiveOvenInBakery();
+        //public IQueryable<Bread> AllSweetsFromBakery(string bakery);
+        //public Bread LightestBread();
+
+        [HttpGet]
+        public IQueryable<Oven> OvensByCapacity(int capacity)
+        {
+            return logic.OvensByCapacity(capacity);
+        }
+
+        [HttpGet]
+        public IQueryable<Bread> BarrelsByMaterial(string bakery)
+        {
+            return logic.AllBreadsFromBakery(bakery);
+        }
+
+        [HttpGet]
+        public Oven MostExpensiveOvenInBakery()
+        {
+            return logic.MostExpensiveOvenInBakery();
+        }
+
+        [HttpGet]
+        public IQueryable<Bread> AllSweetsFromBakery([FromQuery] string bakery)
+        {
+            return logic.AllSweetsFromBakery(bakery);
+        }
+
+        [HttpGet]
+        public Bread BiggestBarrelInWinery()
+        {
+            return logic.LightestBread();
+        }
+    }
+}
