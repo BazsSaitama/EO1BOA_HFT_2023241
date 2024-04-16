833e750df9aae98d74506e649fcb24b9798ecd6c
Szász Balázs <balazs.szasz01@stud.uni-obuda.hu>
2024-04-16 09:54:13 +0200

    Added the Main window + design

---
+++ EO1BOA_GUI_2023242_WPF_Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EO1BOA_GUI_2023242_WPF_Client
+{
+    class MainWindowViewModel
+    {
+    }
+}
